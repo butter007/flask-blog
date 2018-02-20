78f035b9579f796af0272532be9ca06a025955cb
butter007 <33350059+butter007@users.noreply.github.com>
2018-02-20 22:37:23 +0800

    8e
    用户登录确认

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,13 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app(config_name):
@@ -20,6 +22,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
+    login_manager.init_app(app)
     db.init_app(app)
 
     from .main import main as main_blueprint
